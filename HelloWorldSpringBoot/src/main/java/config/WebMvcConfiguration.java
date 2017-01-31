08ae8dab6f8e82956399ddb9ee87a2e83e1ace73
sotheareth <ham8086sotheareth@yahoo.com>
2017-01-31 15:20:47 +0700

    add thymleaf and create session for test session replication

---
+++ HelloWorldSpringBoot/src/main/java/config/WebMvcConfiguration.java
@@ -0,0 +1,47 @@
+package config;
+
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter implements EnvironmentAware {
+	
+	private Environment propertyResolver;
+
+	/**
+	 * Thymeleaf template engine
+	 * @return
+	 */
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+	    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	    templateEngine.addTemplateResolver(templateResolver());
+	    return templateEngine;
+	}
+
+	
+	/**
+	 * View template resolver
+	 * @return
+	 */
+	private ITemplateResolver templateResolver() {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+	    templateResolver.setPrefix("templates/mail/");
+	    templateResolver.setTemplateMode("HTML");
+	    templateResolver.setSuffix(".html");
+	    templateResolver.setOrder(1);
+	    return templateResolver;
+	}
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		propertyResolver = environment;
+	}
+
+}
---
+++ HelloWorldSpringBoot/src/main/java/hello/SessionController.java
@@ -0,0 +1,38 @@
+package hello;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SessionController {
+	
+	@RequestMapping(value ="/greeting",  method = { RequestMethod.GET, RequestMethod.POST })
+	@SuppressWarnings("unchecked")
+    public String greeting(@RequestParam(value="bookName", required = false) String bookName, HttpServletRequest request) {
+		
+		List<String> listOfBooks = (List<String>) request.getSession().getAttribute("Books");
+		if (listOfBooks == null) {
+            listOfBooks = new ArrayList<String>();
+            request.getSession().setAttribute("Books", listOfBooks);
+        }
+		if(bookName != null) {
+			listOfBooks.add(bookName);
+            request.getSession().setAttribute("Books", listOfBooks);
+		}
+        return "greeting";
+        
+    }
+    
+    @RequestMapping("/session")
+    public String session() {
+        return "session";
+    }
+    
+}
